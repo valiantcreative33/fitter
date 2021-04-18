0c63e6c95add64be1cd4f68bc54d716ae50d1bc3
Gabriela Zalta <gzalta96@gmail.com>
2021-04-18 16:14:09 -0500

    trying to deploy to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://gzalta96:lucy0927@cluster0.kubcb.mongodb.net/fitter', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitter', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
