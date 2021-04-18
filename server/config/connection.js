254dd4414b27e851fcab5b1a3dd075e42f10b372
Gabriela Zalta <gzalta96@gmail.com>
2021-04-18 16:04:36 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitter', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://gzalta96:lucy0927@cluster0.kubcb.mongodb.net/fitter', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
